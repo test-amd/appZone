fac298af36310a04c3ca39fd1037dbb2f23db238
test-amd <103923397+test-amd@users.noreply.github.com>
2024-05-12 15:56:13 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 $server_ip = $_SERVER['SERVER_ADDR'];
 echo "Server IP Address is for current instance: " . $server_ip;
-echo "Test from github this time new 3:50";
+echo "Test from github this time new 3:55";
 ?>
